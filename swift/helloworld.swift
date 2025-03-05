63b9db3c0be540216d3614c71d14ebc4a3a48a86
TheSkylerDev <kesch.inno14@gmail.com>
2025-03-05 09:19:18 +0100

    added swift folder with hello world

---
+++ swift/helloworld.swift
@@ -0,0 +1,12 @@
+print("Hewwo, world!")
+//this is a swift program already!
+var num = 5 //that's an integer!
+var num2: Double = 5.0 //that's a double! (the type doesn't have to be specified)
+var num3 = 5.0 //type Double is inferred here
+let pi = 3.14159 //that's a constant! (it can't be changed)
+
+pi = 3.14 //oops! that's a constant! You cannot change it!
+let label = "The width is "
+let width = 94 //let's convert that to a string!
+let widthLabel = label + String(width) //here we go!
+print(widthLabel) //no semicolons needed by the way!
