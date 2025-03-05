08bd691f59ee389f9cd8eff05371b6df1c73d904
TheSkylerDev <kesch.inno14@gmail.com>
2025-03-05 10:26:55 +0100

    Added more stuff for learning

--- swift/helloworld.swift
+++ swift/helloworld.swift
@@ -10,3 +10,47 @@ let label = "The width is "
 let width = 94 //let's convert that to a string!
 let widthLabel = label + String(width) //here we go!
 print(widthLabel) //no semicolons needed by the way!
+
+//let us commence calculation within strings
+let calc = "The result of width times pi i \(Double(width) * pi)"
+print(calc)
+let quote = """
+HEWWO; I'M A SILLY DEER-FOX!
+I WILL NOM YOUR COOKIES!
+
+(double quotes can appear without bein escaped)
+
+Oops!, some Lolcode slipped in here:
+HAI 1.2
+CAN HAS STDIO?
+VISIBLE "Hewwo, world!"
+KTHXBYE
+"""
+print(quote)
+
+var shoppingList = ["catfish", "water", "tulips", "blue paint"] //oops! An array!
+shoppingList[1] = "bottle of water" //let's fix that
+print(shoppingList)
+
+var occupations = [
+    "Malcolm": "Captain",
+    "Gbonjubola Adeosun": "Mekanik"
+]
+occupations["Jayne"] = "Public Relations" //let's add Jayne
+print(occupations)
+
+var empty = []
+var noDict = [:] //empty array and dictionary
+
+//controlling the flow
+let individualScores = [75, 43, 103, 87, 12]
+var teamScore = 0
+for score in individualScores {
+    if score > 50 {
+        teamScore += 3
+    } else {
+        teamScore += 1
+    }
+}
+print(teamScore) //bwoah.
+//let's continue on Mac...
