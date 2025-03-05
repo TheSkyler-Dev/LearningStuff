b80226970a0b9bded079896831080ddf5956891b
TheSkylerDev <kesch.inno14@gmail.com>
2025-03-05 14:13:09 +0100

    Deleted unnecessary stuff, added Kotlin, modified rust

---
+++ kotlin/learn.kt
@@ -0,0 +1,5 @@
+fun main(){
+    println("Hi, Kotlin!")
+    val myVar = "Skyler"
+    prinln("Hi, " + myVar)
+}
--- rust/helloworld.rs
+++ rust/helloworld.rs
@@ -1,4 +1,7 @@
 //rustcomment: easy!
 fn main(){
     println!("HEWWO WORLD!");
+    println!("ROUDS, SKELER, eeh... I mean, RUST!");
+    #[derive(Debug)]
+    struct myStruct(i32);
 }
