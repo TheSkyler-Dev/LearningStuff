e77df51914711b09d76c9fba59bdda6e1304cbc9
TheSkylerDev <kesch.inno14@gmail.com>
2025-03-07 07:30:05 +0100

    Added lambda expression to learn.kt

--- kotlin/learn.kt
+++ kotlin/learn.kt
@@ -1,5 +1,8 @@
-fun main(){
+fun main() {
     println("Hi, Kotlin!")
     val myVar = "Skyler"
     prinln("Hi, " + myVar)
+
+    var upperCaseString = {text: String -> text.uppercase()}
+    println(upperCaseString("hewwo"))
 }
