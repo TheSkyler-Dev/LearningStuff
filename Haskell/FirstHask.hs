47367e5a3dd44bf43dfeab089e17bb01a0c05590
TheSkylerDev <kesch.inno14@gmail.com>
2025-04-01 08:40:47 +0200

    Add Haskell folder

---
+++ Haskell/FirstHask.hs
@@ -0,0 +1,10 @@
+main :: IO()
+main = do
+    putStrLn "Hello, Haskell!"
+    putStrLn "This is my first Haskell program."
+    putStrLn "I am learning functional programming."
+    putStrLn "Haskell is a purely functional programming language."
+    putStrLn "I am excited to learn more about it!"
+    putStrLn "Still getting to grips. Might take a while."
+    let someText = "21" ++ ": Hallo niece and nephew!"
+    putStrLn someText
