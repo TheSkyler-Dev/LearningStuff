775399b71b2b9e4212b1b329d708ee7802da8222
TheSkylerDev <kesch.inno14@gmail.com>
2025-04-03 07:34:21 +0200

    Add fibonacci.hs

---
+++ Haskell/fibonacci.hs
@@ -0,0 +1,15 @@
+import Data.Complex (Complex((:+)))
+
+fibonacci :: [Int] 
+fibonacci = 0 : 1 : zipWith (+) fibonacci (tail fibonacci)
+
+mandelbrot :: [Complex Double]
+mandelbrot = [x :+ y | x <- [-2.0, -1.9 .. 1.0], y <- [-1.0, -0.9 .. 1.0]]
+
+main :: IO()
+main = do
+    putStrLn "Fibonacci sequence:"
+    print $ take 10 fibonacci
+    putStrLn "Mandelbrot set:"
+    print $ take 10 mandelbrot
+    putStrLn "Done!"
