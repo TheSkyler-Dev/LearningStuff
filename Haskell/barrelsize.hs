a2ba8a76033bc598eadfac738f4471ccd0573d6c
TheSkylerDev <kesch.inno14@gmail.com>
2025-04-03 14:02:04 +0200

    Updated barrelsize.hs

--- Haskell/barrelsize.hs
+++ Haskell/barrelsize.hs
@@ -5,6 +5,12 @@ barrelVolume r h = Main.pi * r**2 * h
 barrelSurface :: Double -> Double -> Double
 barrelSurface r h = 2 * Main.pi * r * (r + h)
 
+ccmInLiter :: Double -> Double
+ccmInLiter = (/ 1000) -- convert cubic centimeters in liters
+
+ccmInFlOz :: Double -> Double
+ccmInFlOz = (/ 29.5735) -- convert cubic centimeters in fluid ounces
+
 main :: IO() -- main function: Takes user input and calculates volume and surface area of given barrel
 main = do
     putStrLn "Enter base radius of the barrel: "
@@ -14,10 +20,13 @@ main = do
     let r = read barrelRadius :: Double
     let h = read barrelHeight :: Double
     let volume = barrelVolume r h
+    let liters = ccmInLiter volume
     let surface = barrelSurface r h
 
     putStrLn $ "Base radius of the barrel: " ++ show r
     putStrLn $ "Height of the barrel: " ++ show h
-    putStrLn $ "Volume of the barrel: " ++ show volume
+    putStrLn $ "Volume of the barrel: " ++ show volume ++ " ccm"
+    putStrLn $ "That's " ++ show liters ++ " liters" 
+    putStrLn $ "That's " ++ show (ccmInFlOz volume) ++ " fl. oz."
     putStrLn $ "Surface area of the barrel: " ++ show surface
     putStrLn "Done!"
