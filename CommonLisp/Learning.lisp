f2a76f217342e1359fb95eef82b7c6084e9a131c
TheSkylerDev <kesch.inno14@gmail.com>
2025-05-15 07:48:15 +0200

    Update Learning.lisp

--- CommonLisp/Learning.lisp
+++ CommonLisp/Learning.lisp
@@ -15,3 +15,23 @@
 )
 
 (print-sign 512)
+
+;; Function to return the sum of all elements in a list
+(defun sum-list (l)
+    let ((total 0))
+        (dolist (v l total)
+            (setf total (+ total v))))
+
+(sum-list '(1 2 3 4 5 6 7 8 9 10))
+
+(defun exprl (x) (list '+ (list '* 2 x) 1))
+
+(exprl 3)
+
+(defun epirb (x) `(+ (* 3 ,x) 1))
+
+(epirb 4)
+
+defun expra (&rest xs) `(+ (* 2 ,@xs) 1)
+
+(expra 3 4 6 5 7)
